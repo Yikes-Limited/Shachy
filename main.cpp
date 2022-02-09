b5100eca5e788acd5719bf1f55b19f4d6d157831
PlumpAndProud <f.machi03@gmail.com>
2022-02-09 18:33:58 +0100

    v1.0.0
    - renamed figures
    - figured out 2dim table

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include "Figura.h"
+using namespace std;
 
 using namespace std;
+
 class Figura
 {
 public:
+
     int x; // A - H
     int y; // 1 - 8
+    char kolor; // B/C
     string rodzaj;
     char symbol;
-    char kolor; // B/C
     bool tyl;
     bool zbita;
 
-    Figura(int x = 10, int y = 8, string rodzaj = "xyz", char symbol = '-', char kolor = 'x', bool tyl = true, bool zbita = false)
+    Figura()
+    {
+        this->x = 0;
+        this->y = 0;
+        this->rodzaj = " ";
+        this->symbol = ' ';
+        this->kolor = ' ';
+        this->tyl = false;
+        this->zbita = false;
+    }
+
+    Figura(int x, int y, string rodzaj, char symbol, char kolor, bool tyl, bool zbita)
     {
         this->x = x;
         this->y = y;
@@ -22,27 +37,38 @@ public:
         this->tyl = tyl;
         this->zbita = zbita;
     }
+
+    void przepisz(Figura* figura)
+    {
+        this->x = figura->x;
+        this->y = figura->y;
+        this->rodzaj = figura->rodzaj;
+        this->symbol = figura->symbol;
+        this->kolor = figura->kolor;
+        this->tyl = figura->tyl;
+        this->zbita = figura->zbita;
+    }
 };
-Figura Szachownica[9][8];
-// czarne
-Figura *KrolC = new Figura(4, 1, "krol", 'k', 'c', true, false);
-Figura *DamaC = new Figura(5, 1, "dama", 'd', 'c', true, false);
-Figura *Goniec1C = new Figura(3, 1, "goniec", 'g', 'c', true, false);
-Figura *Goniec2C = new Figura(6, 1, "goniec", 'g', 'c', true, false);
-Figura *Skoczek1C = new Figura(2, 1, "skoczek", 's', 'c', true, false);
-Figura *Skoczek2C = new Figura(7, 1, "skoczek", 's', 'c', true, false);
-Figura *Wieza1C = new Figura(1, 1, "wieza", 'w', 'c', true, false);
-Figura *Wieza2C = new Figura(8, 1, "wieza", 'w', 'c', true, false);
 
 // biale
-Figura *KrolB = new Figura(4, 8, "krol", 'K', 'b', true, false);
-Figura *DamaB = new Figura(5, 8, "dama", 'D', 'b', true, false);
-Figura *Goniec1B = new Figura(3, 8, "goniec", 'G', 'b', true, false);
-Figura *Goniec2B = new Figura(6, 8, "goniec", 'G', 'b', true, false);
-Figura *Skoczek1B = new Figura(2, 8, "skoczek", 'S', 'b', true, false);
-Figura *Skoczek2B = new Figura(7, 8, "skoczek", 'S', 'b', true, false);
-Figura *Wieza1B = new Figura(1, 8, "wieza", 'W', 'b', true, false);
-Figura *Wieza2B = new Figura(8, 8, "wieza", 'W', 'b', true, false);
+Figura* WiezaLB = new Figura(1, 8, "wieza", 'W', 'b', true, false);
+Figura* SkoczekLB = new Figura(2, 8, "skoczek", 'S', 'b', true, false);
+Figura* GoniecLB = new Figura(3, 8, "goniec", 'G', 'b', true, false);
+Figura* KrolB = new Figura(4, 8, "krol", 'K', 'b', true, false);
+Figura* DamaB = new Figura(5, 8, "dama", 'D', 'b', true, false);
+Figura* GoniecPB = new Figura(6, 8, "goniec", 'G', 'b', true, false);
+Figura* SkoczekPB = new Figura(7, 8, "skoczek", 'S', 'b', true, false);
+Figura* WiezaPB = new Figura(8, 8, "wieza", 'W', 'b', true, false);
+
+// czarne
+Figura* WiezaLC = new Figura(1, 1, "wieza", 'w', 'c', true, false);
+Figura* SkoczekLC = new Figura(2, 1, "skoczek", 's', 'c', true, false);
+Figura* GoniecLC = new Figura(3, 1, "goniec", 'g', 'c', true, false);
+Figura *KrolC = new Figura(4, 1, "krol", 'k', 'c', true, false);
+Figura *DamaC = new Figura(5, 1, "dama", 'd', 'c', true, false);
+Figura *GoniecPC = new Figura(6, 1, "goniec", 'g', 'c', true, false);
+Figura *SkoczekPC = new Figura(7, 1, "skoczek", 's', 'c', true, false);
+Figura *WiezaPC = new Figura(8, 1, "wieza", 'w', 'c', true, false);
 
 // Y       BIAŁE
 // 8 |W|S|G|K|D|G|S|W|
@@ -59,5 +85,11 @@ Figura *Wieza2B = new Figura(8, 8, "wieza", 'W', 'b', true, false);
 
 int main()
 {
-    cout << "Printf" << endl;
+	Figura Szachownica[8][8];
+
+	Szachownica[7][7].przepisz(WiezaPB);
+
+	cout << Szachownica[7][7].rodzaj<<endl;
+	cout << Szachownica[7][7].symbol<<endl;
+	cout << Szachownica[0][0].symbol << endl;
 }
