3ab4867632ba3cd12d585d621427b4909da184d9
Helkovsky <kacperkloczek@gmail.com>
2022-02-11 17:29:53 +0100

    + wszystkie figury

--- main.cpp
+++ main.cpp
@@ -83,6 +83,17 @@ Figura *GoniecPC = new Figura(6, 1, "goniec", 'g', 'c', true, false);
 Figura *SkoczekPC = new Figura(7, 1, "skoczek", 's', 'c', true, false);
 Figura *WiezaPC = new Figura(8, 1, "wieza", 'w', 'c', true, false);
 
+// czarne piony
+
+Figura *Pionek1C = new Figura(1, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek2C = new Figura(2, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek3C = new Figura(3, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek4C = new Figura(4, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek5C = new Figura(5, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek6C = new Figura(6, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek7C = new Figura(7, 2, "pionek", 'p', 'c', true, false);
+Figura *Pionek8C = new Figura(8, 2, "pionek", 'p', 'c', true, false);
+
 // biale
 Figura *WiezaLB = new Figura(1, 8, "wieza", 'W', 'b', true, false);
 Figura *SkoczekLB = new Figura(2, 8, "skoczek", 'S', 'b', true, false);
@@ -93,17 +104,26 @@ Figura *GoniecPB = new Figura(6, 8, "goniec", 'G', 'b', true, false);
 Figura *SkoczekPB = new Figura(7, 8, "skoczek", 'S', 'b', true, false);
 Figura *WiezaPB = new Figura(8, 8, "wieza", 'W', 'b', true, false);
 
-Figura *Figury[16]{WiezaLC, SkoczekLC, GoniecLC, KrolC, DamaC, GoniecPC, SkoczekPC, WiezaPC, WiezaLB, SkoczekLB, SkoczekPB, GoniecLB, KrolB, DamaB, GoniecPB, WiezaPB};
+// biale piony
+
+Figura *Pionek1B = new Figura(1, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek2B = new Figura(2, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek3B = new Figura(3, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek4B = new Figura(4, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek5B = new Figura(5, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek6B = new Figura(6, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek7B = new Figura(7, 7, "pionek", 'P', 'b', true, false);
+Figura *Pionek8B = new Figura(8, 7, "pionek", 'P', 'b', true, false);
+
+Figura *Figury[32]{WiezaLC, SkoczekLC, GoniecLC, KrolC, DamaC, GoniecPC, SkoczekPC, WiezaPC, WiezaLB, SkoczekLB, SkoczekPB, GoniecLB, KrolB, DamaB, GoniecPB, WiezaPB, Pionek1B, Pionek2B, Pionek3B, Pionek4B, Pionek5B, Pionek6B, Pionek7B, Pionek8B, Pionek1C, Pionek2C, Pionek3C, Pionek4C, Pionek5C, Pionek6C, Pionek7C, Pionek8C};
 
 int main()
 {
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < 32; i++)
     {
         Figura *Temp = Figury[i];
         Szachownica[Temp->x - 1][Temp->y - 1].przypisz(Temp);
     }
-    rysuj();
-    rusz(0, 0, 3, 3);
     rysuj();
 }
 
